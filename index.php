2934513bcdfbd836b16a2ce65cd2572361e100ec
Yariel1983 <cubanboy4life@gmail.com>
2019-01-26 13:33:53 -0500

    adding styling to landingpage

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
                                 <div class="col-sm">
                                     <input type="text" class="form-control" placeholder="Last name">
                                 </div>
-                                <button class="btn btn-default btn-sm"> <a href="#" target="_blank" >Login</a>
+                                <button class="btn btn-default btn-xl"id="loginbtn"> <a href="#" target="_blank" >Login</a>
                                 </button>
                             </div>
                         </form>
@@ -116,12 +116,109 @@
                         <h2 class="section-heading">Want to make an original Business Card yourself?</h2>
                         <hr class="light">
                         <p class="text-faded">Forget that! Who would ever want to put in all of that effort for a website? Just open up your web browser and type "bootstrap template" into your favorite search engine, like Yahoo! or Bing, and you're on your way! There are hundreds of templates to choose from, but go ahead and pick this same exact template from the first result on google, edit a few lines, and you're on your way! No one will notice!</p>
-                        <button class="btn btn-default btn-xl"> <a href="#" target="_blank" >Register</a>
+                        <button class="btn btn-default btn-xl" id="registerbtn"> <a href="#" target="_blank" >Register</a>
                         </button>
-
                     </div>
                 </div>
             </div>
         </section>
-    </body>
+
+        <section id="services">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading">So Fucking Creative</h2>
+                        <hr class="primary">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-globe wow bounceIn text-primary" style="visibility: visible; animation-name: bounceIn;"></i>
+                            <h3>This Fucking Template</h3>
+                            <p class="text-muted">Guaranteed to use the same fucking template that every other bootstrap website uses, downloaded straight from The Web™</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s" style="visibility: visible; animation-delay: 0.1s; animation-name: bounceIn;"></i>
+                            <h3>This Set of Four Icons</h3>
+                            <p class="text-muted">Look at this cool set of four icons describing different things about us! We use four, because it's the default.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-thumbs-up wow bounceIn text-primary" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: bounceIn;"></i>
+                            <h3>Lots of effort</h3>
+                            <p class="text-muted">We even changed some of the icons! We take the extra effort to make our designs truly original.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: bounceIn;"></i>
+                            <h3>Made with Love</h3>
+                            <p class="text-muted">Because nothing says hard work and talent like editing a few lines of text.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!----------- Footer ------------>
+        <footer class="footer-bs" id="test">
+            <div class="row">
+                <div class="col-md-3 footer-brand animated fadeInLeft">
+                    <h2>Logo</h2>
+                    <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
+                    <p>© 2014 BS3 UI Kit, All rights reserved</p>
+                </div>
+                <div class="col-md-4 footer-nav animated fadeInUp">
+                    <h4>Menu —</h4>
+                    <div class="col-md-6">
+                        <ul class="pages">
+                            <li><a href="#">Travel</a></li>
+                            <li><a href="#">Nature</a></li>
+                            <li><a href="#">Explores</a></li>
+                            <li><a href="#">Science</a></li>
+                            <li><a href="#">Advice</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="list">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contacts</a></li>
+                            <li><a href="#">Terms & Condition</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 footer-social animated fadeInDown">
+                    <h4>Follow Us</h4>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">RSS</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 footer-ns animated fadeInRight">
+                    <h4>Newsletter</h4>
+                    <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
+                    <p>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button"><i class="far fa-envelope"></i></span></button>
+                        </span>
+                    </div><!-- /input-group -->
+                    </p>
+                </div>
+            </div>
+        </footer>
+        <section style="text-align:center; margin:10px auto;"><p>Designed by <a href="http://enfoplus.net">Yariel Dominguez</a></p></section>
+
+    </div>
+
+</body>
 </html
