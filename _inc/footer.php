53479cbebe7630b49caf9d4a7a3bedfb4337395d
Yariel1983 <cubanboy4life@gmail.com>
2019-02-07 15:54:51 -0500

    merge

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,46 +1,41 @@
 <!----------- Footer ------------>
-<footer class="footer-bs" id="carousel1">
+<hr>
+<footer class="footer-bs text-dark">
     <div class="row">
         <div class="col-md-3 footer-brand animated fadeInLeft">
-            <span><i class="fas fa-cloud text-white fa-2x mb-2 mr-2"></i>Virtual Business Card</span>
-            <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
-            <p>© 2019 Yariel Dominguez UI, All rights reserved</p>
+            <span><i class="fas fa-cloud text-primary fa-2x mb-2 mr-2"></i>Virtual Business Card</span>
+            <p class="text-dark">Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
+            <p class="text-dark">© 2019 Yariel Dominguez UI, All rights reserved</p>
         </div>
         <div class="col-md-4 footer-nav animated fadeInUp">
-            <h4>Menu —</h4>
+            <h4>Info —</h4>
             <div class="col-md-6">
-                <ul class="pages">
-                    <li><a href="#">Designs</a></li>
-                    <li><a href="#">Category</a></li>
-                    <li><a href="#">Demo</a></li>
-                    <li><a href="#">Donate</a></li>
-                    <li><a href="#">Advice</a></li>
-                </ul>
+                
             </div>
             <div class="col-md-6">
                 <ul class="list">
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="#">Terms & Condition</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a class="text-dark" href="about.php">About Us</a></li>
+                    <li><a class="text-dark" href="contact.php">Contact</a></li>
+                    <li><a class="text-dark" href="#">Terms & Condition</a></li>
+                    <li><a class="text-dark" href="#">Privacy Policy</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-md-2 footer-social animated fadeInDown">
             <h4>Follow Us</h4>
             <ul>
-                <li><a href="#"><span class="fab fa-facebook fa-2x"></span></a></li>
-                <li><a href="#"><span class="fab fa-instagram fa-2x"></span></a></li>
-                <li><a href="#"><span class="fab fa-twitter fa-2x"></span></a></li>
-                <li><a href="#"><span class="fab fa-linkedin fa-2x"></span></a></li>
+                <li><a href="#"><span class="fab fa-facebook fa-2x text-primary"></span></a></li>
+                <li><a href="#"><span class="fab fa-instagram fa-2x text-danger"></span></a></li>
+                <li><a href="#"><span class="fab fa-twitter fa-2x text-primary"></span></a></li>
+                <li><a href="#"><span class="fab fa-linkedin fa-2x text-primary"></span></a></li>
             </ul>
         </div>
         <div class="col-md-3 footer-ns animated fadeInRight">
             <h4>Newsletter</h4>
-            <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
+            <p class="text-dark">A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
             <p>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search">
+                <input type="text" class="form-control" placeholder="Search" id="footersearchbtn">
                 <span class="input-group-btn">
                     <button class="btn btn-light ml-0" type="button" id="footerbtn"><i class="fas fa-search"></i></span></button>
                 </span>
@@ -49,7 +44,7 @@
         </div>
     </div>
 </footer>
-<section style="text-align:center; margin:10px auto;"><p>Designed by <a href="http://enfoplus.net">Yariel Dominguez Freelance Frontend Web Developer</a></p></section>
+<section style="text-align:center; margin:10px auto;"><p>Designed by <a href="https://github.com/yariel1983">Yariel Dominguez Freelance Frontend Web Developer</a></p></section>
 
 <!-- Jquery -->
 <script src="assets/js/jquery-3.3.1.slim.min.js"></script>
--- _inc/header.php
+++ _inc/header.php
@@ -7,35 +7,38 @@
         <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
         <link rel="stylesheet" href="assets/css/font.googleapis.css">        
         <link rel="stylesheet" href="assets/css/fontawesome/css/all.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
     </head>
     <body>
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-            <a class="navbar-brand text-white" href="index.php"><span><i class="fas fa-cloud text-white fa-2x mr-2" href="index.php"></i></span>Virtual Business Card</a>
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <a class="navbar-brand text-dark" href="index.php"><span><i class="fas fa-cloud text-primary fa-2x mr-2" href="index.php"></i></span>Virtual Business Card</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home
+                        <a class="nav-link text-dark" href="index.php">Home
                             <span class="sr-only">(current)</span>
                         </a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="about.php">About</a>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="about.php">About</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="services.php">Design</a>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="services.php">Design</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="contact.php">Contact</a>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="contact.php">Contact</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="cart.php"><span><i class="fas fa-shopping-cart text-white" id="carticon"></i></span>
+                    <li class="nav-item text-dark">
+                        <a class="nav-link text-dark" href="cart.php"><span><i class="fas fa-shopping-cart text-dark" id="carticon"></i></span>
                             <span class="badge badge-pill badge-danger text-danger" id="cartnoti">0</span>
                         </a>
                     </li>
+                    
                 </ul>
             </div>
         </nav>
+     <!--Navigation-->   
