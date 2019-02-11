6aa37705a5a35b9ae35bd1c6da0b859f44578a22
Yariel1983 <cubanboy4life@gmail.com>
2019-02-11 16:07:56 -0500

    about

--- about.php
+++ about.php
@@ -22,11 +22,11 @@
             <div class="col-lg-4"></div>
             <div class="col-lg-4">
                 <div class="testimonialItem mx-auto mt-5 mb-5 mb-lg-0">
-                    <h5 class="font-weight-bold">About Us</h5>
+                    <!--<h5 class="font-weight-bold">About Us</h5>
                      <p class="font-weight-light mb-5 text-justify">"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
                          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."</p>
-                 </div> 
+                 </div> -->
                 </div>
                 <div class="col-lg-4"></div>
             </div>
