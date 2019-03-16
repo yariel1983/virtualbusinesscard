8f15b047e9fae4fa260f6810bd6bce9702415aa8
Yariel1983 <cubanboy4life@gmail.com>
2019-03-16 00:38:15 -0400

    Working on services.php

--- services.php
+++ services.php
@@ -1,21 +1,16 @@
 <?php include('_inc/header.php'); ?>
 <!--Page Content-->
-<div class="container-fluid">
-    <div class="container portfolio mb-3">&nbsp;
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="heading">				
-                    <span><i class="fas fa-cloud text-primary fa-2x ml-2 mr-2" href="index.php"></i>Virtual Business Card</span>
-                </div>
-            </div>	
-        </div>
+<div class="container-fluid" id="servdiv">
+    <div class="container portfolio mb-5">&nbsp;
+
         <div class="bio-info">
             <div class="row">
                 <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
                     <div class="row">
                         <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
                             <div class="bio-image">
-                                <img src="http://placehold.it/350x400" class="img-responsive" alt="a" />                            
+                                <img src="http://placehold.it/350x400" class="img-responsive" alt="a" id="servicesimg"/>
+
                             </div>
                         </div>
                     </div>	
@@ -42,6 +37,10 @@
                                 <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12 mt-2">
                                     <input type="text" class="form-control" placeholder="fax">
                                 </div>
+                                <div class="col-sm-2 col-md-3 col-lg-4 col-xs-2 mt-2">
+                                    <input type="file" class="form-control-file" id="choosepic">
+                                </div>
+
                             </div>
                         </form>
 
@@ -61,30 +60,37 @@
                                     <a class="gplus-ic">
                                         <i class="fab fa-instagram fa-lg text-danger fa-2x mt-5"> </i>
                                     </a>   
-                                </div>
+                                </div>                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>            
         </div>
-        <!--buttons-->
-        <div class="container heading text-center mt-5">
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="form-group text-center">                       
-                        <input type="file" class="form-control-file btn btn-light bg-transparent border-0" id="exampleFormControlFile1">
-                    </div>
-                </div>
-                <div class="col-sm-2">
-                    <a class="btn btn-danger" href="#">Reset</a>
-                </div>
-                <div class="col-sm-2">
-                    <a class="btn btn-primary" href="checkout.php">Save</a>
-                </div>
-
-
-            </div>  
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12 mt-5">
+            <a class="btn-outline-light text-dark" href="#" id="resetbtn"> Preview <i class="fas fa-eye"></i></a>
+            <a class="btn-outline-light text-dark" href="checkout.php" id="savebtn"> E_Print <i class="fas fa-shopping-cart"></i></a>
         </div>
+
     </div>   
+
+
+
     <?php include('_inc/footer.php'); ?>
+
+
+
+
+    <!--buttons
+                            
+                        <input type="file" class="form-control-file" id="choosepic">
+                  
+                    <button class="btn btn-outline-dark" id="resetbtn"> Preview</button>
+               
+
+                
+                    <button class="btn btn-outline-dark" id="resetbtn"> Reset</button>
+                
+
+               
+                    <button class="btn btn-dark btn-outline" herf="payment.php" id="savebtn"> Save</button>-->
