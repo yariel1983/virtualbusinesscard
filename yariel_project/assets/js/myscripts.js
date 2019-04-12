e0cc8f8761e37a21d9973698c23ce927349e0d60
Paul Ferra <paul@etags.com>
2019-04-12 11:20:37 -0400

    Index & myScript - Updated

--- yariel_project/assets/js/myscripts.js
+++ yariel_project/assets/js/myscripts.js
@@ -1,4 +1,14 @@
-$(".tile-front").click(function () {
-       $(".tile-front").hide();
-       $(".tile-back").show();
+$(document).ready(function () {
+//Aqui pon tu codigo
+
+    $(".tile-front").click(function () {
+        $(".tile-front").hide();
+        $(".tile-back").show();
     });
+
+    $(".tile-back").click(function () {
+        $(".tile-back").hide();
+        $(".tile-front").show();
+    });
+
+});
--- yariel_project/index.php
+++ yariel_project/index.php
@@ -3,48 +3,31 @@
 
 <div class="container mt-5">&nbsp;
     <!-- One tile Front-->
-
-    <div class="tile">
-
-        <div class="card tile-front">
-
-            <div class="bg-tile">
-
-                <i class="material-icons m_emote ml-3">account_box</i>
-
+    <div class="row">
+        <div class="tile">
+            <div class="card tile-front">
+                <div class="bg-tile">
+                    <i class="material-icons m_emote ml-3">account_box</i>
+                </div>
+                <div class="card-body">
+                    <p>5 Users</p>
+                </div>
+                <div class="card-footer text-muted m_desc">
+                    Online
+                </div>
             </div>
 
-            <div class="card-body">
-
-                <p>5 Users</p>
-
-            </div>
-
-            <div class="card-footer text-muted m_desc">
-
-                Online
-
-            </div>
-
-        </div>
-        
             <!-- (Back Tile) -->
-        <div class="card tile-back">
-
-            <div class="card-body">
-
-                <p>User online correspond to people connected on your network</p>
-
+            <div class="card tile-back" style="display:none">
+                <div class="card-body">
+                    <p>User online correspond to people connected on your network</p>
+                </div>
             </div>
-
         </div>
-
     </div>
-
     <!-- End Of Tile-->
-
 </div>
-<!-- (Footer) -->    
+<!-- (Footer) -->   
 <?php
 include('_inc/footer.php');
 
