47a2a446192ef64e6517c9eb0c321f9122482514
Yariel1983 <cubanboy4life@gmail.com>
2019-04-17 17:34:39 -0400

    adding meta tag

--- _inc/header.php
+++ _inc/header.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="assets/css/fontawesome/css/all.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
         <link rel="shortcut icon" href="assets/images/yariel.png">
+        <meta charset="utf-8">
     </head>
 
     <body id="page-top">
